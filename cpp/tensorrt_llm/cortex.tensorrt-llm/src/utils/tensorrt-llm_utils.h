221667d99f158e92e7fbb2991d127b46cb316408
CameronNg <huyhoang.nguyen130820@gmail.com>
2024-06-17 03:54:26 +0700

    fix: winsock2.h header error v3

--- cpp/tensorrt_llm/cortex.tensorrt-llm/src/utils/tensorrt-llm_utils.h
+++ cpp/tensorrt_llm/cortex.tensorrt-llm/src/utils/tensorrt-llm_utils.h
@@ -13,11 +13,7 @@
 #include <vector>
 // Include platform-specific headers
 #ifdef _WIN32
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#include <winsock2.h>
 #include <windows.h>
-#endif
 #else
 #include <dirent.h>
 #endif
