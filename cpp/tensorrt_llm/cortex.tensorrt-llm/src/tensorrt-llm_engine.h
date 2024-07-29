163b3c07d0ea7b79006a085fbd98add05b41ff28
sangjanai <sang@jan.ai>
2024-07-29 06:25:44 +0000

    fix: add formatter

--- cpp/tensorrt_llm/cortex.tensorrt-llm/src/tensorrt-llm_engine.h
+++ cpp/tensorrt_llm/cortex.tensorrt-llm/src/tensorrt-llm_engine.h
@@ -7,62 +7,56 @@
 #include <queue>
 #include <string>
 
+#include <nlohmann/json.hpp>
 #include "NvInfer.h"
 #include "base/cortex-common/enginei.h"
+#include "cpp-tiktoken/emdedded_resource_reader.h"  //include to use tiktoken
+#include "cpp-tiktoken/encoding.h"                  //include to use tiktoken
 #include "models/chat_completion_request.h"
 #include "models/load_model_request.h"
 #include "sentencepiece_processor.h"
-#include "cpp-tiktoken/encoding.h" //include to use tiktoken
-#include "cpp-tiktoken/emdedded_resource_reader.h" //include to use tiktoken
 #include "tensorrt_llm/plugins/api/tllmPlugin.h"
 #include "tensorrt_llm/runtime/generationInput.h"
 #include "tensorrt_llm/runtime/generationOutput.h"
 #include "tensorrt_llm/runtime/gptJsonConfig.h"
-#include "tensorrt_llm/runtime/modelConfig.h"
 #include "tensorrt_llm/runtime/gptSession.h"
+#include "tensorrt_llm/runtime/modelConfig.h"
 #include "tensorrt_llm/runtime/samplingConfig.h"
 #include "tensorrt_llm/runtime/tllmLogger.h"
 #include "trantor/utils/ConcurrentTaskQueue.h"
 #include "trantor/utils/Logger.h"
-#include <nlohmann/json.hpp>
-
 
 using namespace tensorrt_llm::runtime;
 // This class is file source reader from https://github.com/gh-markt/cpp-tiktoken/blob/master/ut/tests.cpp
 class TFilePathResourceReader : public IResourceReader {
-public:
-    TFilePathResourceReader(const std::string& path) 
-        : path_(path)
-    {
+ public:
+  TFilePathResourceReader(const std::string& path) : path_(path) {}
+
+  std::vector<std::string> readLines() override {
+    std::ifstream file(path_);
+    if (!file.is_open()) {
+      throw std::runtime_error("Embedded resource '" + path_ + "' not found.");
     }
 
-    std::vector<std::string> readLines() override {
-        std::ifstream file(path_);
-        if (!file.is_open()) {
-            throw std::runtime_error("Embedded resource '" + path_ + "' not found.");
-        }
-
-        std::string line;
-        std::vector<std::string> lines;
-        while (std::getline(file, line)) {
-            lines.push_back(line);
-        }
-
-        return lines;
+    std::string line;
+    std::vector<std::string> lines;
+    while (std::getline(file, line)) {
+      lines.push_back(line);
     }
-private:
-    std::string path_;
+
+    return lines;
+  }
+
+ private:
+  std::string path_;
 };
 
 class Tokenizer {
 
  public:
-  Tokenizer() {
-  }
+  Tokenizer() {}
 
-  virtual std::string DecodeWithSpace(const int id) {
-    return "";
-  }
+  virtual std::string DecodeWithSpace(const int id) { return ""; }
 
   virtual std::string Decode(const std::vector<int32_t> ids) = 0;
 
@@ -73,11 +67,12 @@ class SentencePieceTokenizer : public Tokenizer {
  private:
   sentencepiece::SentencePieceProcessor processor;
 
-  void ReplaceSubstring(std::string& base, const std::string& from, const std::string& to) {
+  void ReplaceSubstring(std::string& base, const std::string& from,
+                        const std::string& to) {
     size_t start_pos = 0;
     while ((start_pos = base.find(from, start_pos)) != std::string::npos) {
-        base.replace(start_pos, from.length(), to);
-        start_pos += to.length();
+      base.replace(start_pos, from.length(), to);
+      start_pos += to.length();
     }
   }
 
@@ -115,7 +110,8 @@ class TiktokenTokenizer : public Tokenizer {
  public:
   TiktokenTokenizer(const std::string& model_path) : Tokenizer() {
     TFilePathResourceReader reader(model_path);
-    encoder = GptEncoding::get_encoding_llama3(LanguageModel::CL100K_BASE, &reader);
+    encoder =
+        GptEncoding::get_encoding_llama3(LanguageModel::CL100K_BASE, &reader);
     LOG_INFO << "Successully loaded the tokenizer";
   }
 
@@ -129,26 +125,24 @@ class TiktokenTokenizer : public Tokenizer {
     return ids;
   }
 };
-  enum class ModelType {
-    kOpenHermes, kLlama3, kMistral
-};
+enum class ModelType { kOpenHermes, kLlama3, kMistral };
 
 struct InferenceState {
   int prev_pos{0};
   bool is_finished;
   std::queue<std::string> texts_to_stream;
-  std::mutex queue_mutex; // Mutex to protect access to textsToStream
+  std::mutex queue_mutex;  // Mutex to protect access to textsToStream
   size_t stop_word_match_len = 0;
-  std::vector<std::string> sequence_openhermes = {"<", "|", "im", "_", "end", "|", ">"};
+  std::vector<std::string> sequence_openhermes = {"<",   "|", "im", "_",
+                                                  "end", "|", ">"};
   std::vector<std::string> sequence_mistral = {"[", "INST", "]"};
   int token_gen_count = 0;
 
-  void Reset() {
-    stop_word_match_len = 0;
-  }
+  void Reset() { stop_word_match_len = 0; }
 
   bool IsComplete(ModelType model_type) const {
-    if(model_type == ModelType::kOpenHermes || model_type == ModelType::kLlama3) {
+    if (model_type == ModelType::kOpenHermes ||
+        model_type == ModelType::kLlama3) {
       return stop_word_match_len >= sequence_openhermes.size();
     } else {
       return stop_word_match_len >= sequence_mistral.size();
@@ -156,17 +150,16 @@ struct InferenceState {
   }
 
   const std::string& GetSequence(ModelType model_type, size_t index) {
-    if(model_type == ModelType::kOpenHermes || model_type == ModelType::kLlama3) {
+    if (model_type == ModelType::kOpenHermes ||
+        model_type == ModelType::kLlama3) {
       return sequence_openhermes[index];
     } else {
       return sequence_mistral[index];
     }
-
   }
 };
 
 namespace tensorrtllm {
-
 
 class TensorrtllmEngine : public EngineI {
  public:
@@ -188,7 +181,8 @@ class TensorrtllmEngine : public EngineI {
       std::shared_ptr<Json::Value> json_body,
       std::function<void(Json::Value&&, Json::Value&&)>&& callback) final;
   virtual std::vector<int> EncodeHeaderLlama3(const std::string& role);
-  virtual std::vector<int> EncodeMessageLlama3( const std::string& role, const std::string& content);
+  virtual std::vector<int> EncodeMessageLlama3(const std::string& role,
+                                               const std::string& content);
   // API to get running models.
   void GetModels(
       std::shared_ptr<Json::Value> json_body,
@@ -221,4 +215,4 @@ class TensorrtllmEngine : public EngineI {
   ModelType model_type_ = ModelType::kOpenHermes;
 };
 
-} // namespace inferences
+}  // namespace tensorrtllm
